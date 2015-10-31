9830a6f50788586854d582911c8d57a5b365b1bd
whatrocks <charles.harrington@gmail.com>
2015-10-30 17:14:18 -0700

    Initial mysql database constructed. Loaded into local server.

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,30 @@
 CREATE DATABASE chat;
 
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  id_users INT NOT NULL,
+  text VARCHAR(255),
+  id_rooms INT NOT NULL,
+  created_at DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_users) REFERENCES users(id),
+  FOREIGN KEY (id_rooms) REFERENCES rooms(id)
+);
+
 
 
 
